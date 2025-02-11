998ce11bb0b68362212af2292955caab616d5ddc
sutsuk <107033259+sutsuk@users.noreply.github.com>
2025-02-11 15:59:43 +0900

    Add settings.php

---
+++ devel/settings.php
@@ -0,0 +1,25 @@
+<?php
+  $Debug = FALSE;
+  $HTTPSOnly = TRUE;
+  $DomainName = "www.momiji.ddns-ip.net";
+  $DomainOnly = TRUE;
+  $IPAddress = "172.23.72.58";
+  $IPOnly = FALSE;
+  $Generate = TRUE;
+  $PublicMode = TRUE;
+  $default_backgrounds = array();
+  $default_backgrounds[] = "/public-background.webp";  
+  $default_background_public = "/public-background.webp";
+  $img_exts = array();
+  $img_exts[] = "png";
+  $img_exts[] = "jpg";
+  $img_exts[] = "jpeg";
+  $img_exts[] = "bmp";
+  $img_exts[] = "webp";
+  $msg = array();
+  array_push($msg, "[INFO] texのコンパイルは");
+  array_push($msg, "zipでまとめたものでも可能になりました．");
+  $notice = array();
+  $warn = array();
+  date_default_timezone_set('Asia/Tokyo');
+?>
